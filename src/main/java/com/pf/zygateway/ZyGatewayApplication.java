d442fff3efce41cba4a3defdf8a1beb68e71664b
彭飞 <pengfei@oschina.cn>
2023-11-13 16:35:51 +0800

    网关聚合swagger

---
+++ src/main/java/com/pf/zygateway/ZyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pf.zygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZyGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pf/zygateway/ZyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pf.zygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
